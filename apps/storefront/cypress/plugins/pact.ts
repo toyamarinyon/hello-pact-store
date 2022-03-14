f9599d002d2dacf273802dc61f58b039b26f3d53
toyamarinyon <toyamarinyon@gmail.com>
2022-03-15 01:58:10 +0900

    update

--- apps/storefront/cypress/plugins/pact.ts
+++ apps/storefront/cypress/plugins/pact.ts
@@ -1,6 +1,5 @@
 // This would be externalised into a separate, importable plugin
 import { Pact } from "@pact-foundation/pact";
-import rimraf from "rimraf";
 
 /**
  * @type {Cypress.PluginConfig}
@@ -29,39 +28,6 @@ const handler = (on: (event: string, config: any) => void) => {
         "pact: cannot stop server, as no Pact mock service has been configured"
       );
     },
-    //   async stopMockServer() {
-    //     if (server) {
-    //       return server.finalize().then(() => true);
-    //     }
-    //     throw new Error(
-    //       "pact: cannot stop server, as no Pact mock service has been configured"
-    //     );
-    //   },
-    //   addMockRoute(options) {
-    //     return server.addInteraction(options);
-    //   },
-    //   verifyPacts() {
-    //     if (server) {
-    //       return server.verify();
-    //     }
-    //     throw new Error(
-    //       "pact: cannot verify pacts, as no Pact mock service has been configured"
-    //     );
-    //   },
-    //   clearPreviousPactInteractions({ dir }) {
-    //     return new Promise((resolve, reject) => {
-    //       rimraf(`${dir}/*.json`, (e) => {
-    //         if (e) {
-    //           console.log(
-    //             "pact: error cleaning previous contract files:",
-    //             e.message
-    //           );
-    //           reject(e);
-    //         }
-    //         resolve(null);
-    //       });
-    //     });
-    //   },
   });
 };
 export default handler;
